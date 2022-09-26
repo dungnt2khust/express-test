311a1df69bff8d7d4a2efb0fbc8f5cc49bafee1d
dungnt2khust <63250067+dungnt2khust@users.noreply.github.com>
2022-09-26 23:30:39 +0700

    1 file modified

--- index.js
+++ index.js
@@ -3,30 +3,7 @@ const app = express();
 const port = 3000;
 var cors = require('cors');
 // Add headers before the routes are defined
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-Requested-With,content-type'
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
-app.use(cors({ origin: true, Credential: true }));
+app.use(cors({ origin: 'https://test-vue-eddieonthecode.firebaseapp.com'));
 
 app.get('/', (req, res) => {
   var rand = Math.random();
