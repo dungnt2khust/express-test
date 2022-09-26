1ee3d68e5b976ae6139365cc79b380be69e15efe
dungnt2khust <63250067+dungnt2khust@users.noreply.github.com>
2022-09-26 22:50:03 +0700

    1 file modified

--- index.js
+++ index.js
@@ -2,18 +2,36 @@ const express = require('express');
 const app = express();
 const port = 3000;
 var cors = require('cors');
-app.use(
-  cors({
-    origin: true,
-    Credential: true,
-  })
-);
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+app.use(cors({ origin: true, Credential: true }));
 
 app.get('/', (req, res) => {
-  var cookie = req?.headers?.cookie;
-  var random = Math.random();
-  res.setHeader('set-cookie', `random=${random}`); // options is optional
-  res.send('Cookie add' + ` random=${random}`);
+  var rand = Math.random();
+  res.setHeader('set-cookie', `random=${rand}`);
+  res.send('Cookie ' + `random=${rand}`);
 });
 
 app.listen(port, () => {
