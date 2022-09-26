1c99948c106190a25c71d7433b8b930257fe9b10
dungnt2khust <63250067+dungnt2khust@users.noreply.github.com>
2022-09-26 16:25:55 +0700

    1 file modified

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000;
 var cors = require('cors');
 const corsConfig = {
   credentials: true,
-  origin: 'https://test-vue-eddieonthecode.firebaseapp.com/',
+  origin: true,
 };
 app.use(cors(corsConfig));
 app.get('/', (req, res) => {
