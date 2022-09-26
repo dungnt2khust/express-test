e1a80bd48c4f0b2a19893c5e5e3636dab9e35c46
dungnt2khust <63250067+dungnt2khust@users.noreply.github.com>
2022-09-26 23:31:53 +0700

    1 file modified

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 var cors = require('cors');
 // Add headers before the routes are defined
-app.use(cors({ origin: 'https://test-vue-eddieonthecode.firebaseapp.com'));
+app.use(cors({ origin: 'https://test-vue-eddieonthecode.firebaseapp.com' }));
 
 app.get('/', (req, res) => {
   var rand = Math.random();
