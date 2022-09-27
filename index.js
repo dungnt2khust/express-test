58ddc329d513b3062b40123965427a374fce5d74
dungnt2khust <63250067+dungnt2khust@users.noreply.github.com>
2022-09-27 10:08:37 +0700

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -2,12 +2,27 @@ const express = require('express');
 const app = express();
 const port = 3000;
 var cors = require('cors');
+var fs = require('fs');
+var data = fs.readFileSync('data.json', 'utf8');
+var words = [];
 // Add headers before the routes are defined
 app.use(cors({ origin: 'https://test-vue-eddieonthecode.firebaseapp.com' }));
 
 app.get('/', (req, res) => {
+  var data = fs.readFileSync('data.json', 'utf8');
+  words = JSON.parse(data);
   var cookie = req?.headers?.cookie;
-  res.send('Cookie ' + cookie);
+  if (cookie) res.send('Cookie ' + cookie);
+});
+
+app.post('/', (req, res) => {
+  var cookie = req?.headers?.cookie;
+  if (cookie == '050201') {
+    var data = fs.readFileSync('data.json', 'utf8');
+    words = JSON.parse(data);
+    words.push(new Date());
+    fs.writeFile('data.json', JSON.stringify(words), 'utf8');
+  }
 });
 
 app.listen(port, () => {
